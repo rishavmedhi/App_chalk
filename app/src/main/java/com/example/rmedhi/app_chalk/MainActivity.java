247ef4c971d32d5bd4ad2dc4eb45a9b923c05b33
rishavmedhi <rishav@chalkstreet.com>
2017-05-08 21:23:51 +0530

    successfully did retrofit :)

--- app/src/main/java/com/example/rmedhi/app_chalk/MainActivity.java
+++ app/src/main/java/com/example/rmedhi/app_chalk/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.rmedhi.app_chalk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 
+import com.example.rmedhi.app_chalk.api.Poll_service;
+import com.example.rmedhi.app_chalk.fetch_api.Fetch_api;
+import com.example.rmedhi.app_chalk.send_api.Send_api;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -44,5 +62,73 @@ public class MainActivity extends AppCompatActivity {
         password_text.setVisibility(rlayout.INVISIBLE);
         retype_pass_text.setVisibility(rlayout.INVISIBLE);
         log_action_btn.setVisibility(rlayout.INVISIBLE);
+
+        // setting up sign up btn action
+        sign_up_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                sign_up_btn.setVisibility(rlayout.INVISIBLE);
+                login_btn.setVisibility(rlayout.INVISIBLE);
+                email_text.setVisibility(rlayout.VISIBLE);
+                password_text.setVisibility(rlayout.VISIBLE);
+                retype_pass_text.setVisibility(rlayout.VISIBLE);
+                log_action_btn.setVisibility(rlayout.VISIBLE);
+
+            }
+        });
+
+        // setting up login button action
+        login_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                sign_up_btn.setVisibility(rlayout.INVISIBLE);
+                login_btn.setVisibility(rlayout.INVISIBLE);
+                email_text.setVisibility(rlayout.VISIBLE);
+                password_text.setVisibility(rlayout.VISIBLE);
+                log_action_btn.setVisibility(rlayout.VISIBLE);
+            }
+        });
+
+        log_action_btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                String user_email=email_text.getText().toString();
+                String user_password=password_text.getText().toString();
+                int flag=0;
+                if (user_password.equals(retype_pass_text.getText().toString())==true)
+                {
+                    flag=user_signup(user_email,user_password);
+                }
+                Log.d("hello:",flag+"");
+            }
+        });
     }
+
+    protected int user_signup(String user_email, String user_password)
+    {
+        Log.d("email",user_email);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://52.77.240.129/rishav/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        Poll_service service = retrofit.create(Poll_service.class);
+
+        Call<Fetch_api> fetch_obj = service.api_user_signup(user_email,user_password);
+        fetch_obj.enqueue(new Callback<Fetch_api>() {
+            @Override
+            public void onResponse(Call<Fetch_api> call, Response<Fetch_api> response) {
+                response.body(); // have your all data
+                String uid =response.body().getUid();
+                String status=response.body().getStatus();
+            }
+
+            @Override
+            public void onFailure(Call<Fetch_api> call, Throwable t) {
+                Log.d("failure",t.toString());
+            }
+        });
+        return 1;
+    }
+
 }
---
+++ app/src/main/java/com/example/rmedhi/app_chalk/Pollit.java
@@ -0,0 +1,23 @@
+package com.example.rmedhi.app_chalk;
+
+/**
+ * Created by R Medhi on 08-05-2017.
+ */
+
+public class Pollit {
+    private String status;
+    private String msg;
+    private String uid;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+}
